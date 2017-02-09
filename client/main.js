b8c7c1d0beafd7e99dc846357e3cd7fac7166199
Edouard MLT <edouard.malot@gmail.com>
2017-02-09 17:13:56 +0100

    First commit with header

---
+++ client/main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './components/app';
+
+
+Meteor.startup(() => {
+  ReactDom.render(<App />, document.querySelector('.render-target'));
+});
